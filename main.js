495a72a26a12078de70f50b6d516189f34e57484
Becks Maybury <becksmaybury@hotmail.co.uk>
2021-05-10 22:08:53 +0100

    Added functionality to fetch ingredients and health info

--- main.js
+++ main.js
@@ -34,17 +34,17 @@ function handleFoodChange() {
 //--- write your code below ---
 
 async function fetchRecipe(food) {
-    let response = await fetch(`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`);
-    let result = await response.json();
-    let recipeList = await result.hits;
-    // console.log(recipeList);
-    return recipeList;
-    //--- write your code above ---
-  }
-  
-  let ulRecipeList = document.querySelector('#recipe-list');
-  
-  async function createRecipeList() {
+  let response = await fetch(`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`);
+  let result = await response.json();
+  let recipeList = await result.hits;
+  // console.log(recipeList);
+  return recipeList;
+  //--- write your code above ---
+}
+
+let ulRecipeList = document.querySelector('#recipe-list');
+
+async function createRecipeList() {
     let items = await fetchRecipe(foodToSearch);
     console.log(items);
     for (let i=0; i<items.length; i++){
@@ -52,15 +52,37 @@ async function fetchRecipe(food) {
       // create recipe section
       let liSection = document.createElement("section");
       liSection.classList.add("item-section");
+      // create image
+      let liItemImage = document.createElement("img");
+      liItemImage.src = items[i].recipe.image;
+      // create recipe details section
+      let liSubSection = document.createElement("section");
+      liSubSection.classList.add("item-details")
       // create recipe title
       let liItem = document.createElement("li");
       liItem.innerText = items[i].recipe.label;
       liItem.classList.add("item-header");
       liItem.id = i;
       console.log("list item added");
-      // create image
-      let liItemImage = document.createElement("img");
-      liItemImage.src = items[i].recipe.image;
+      // create show ingredients link
+      let liItemIngredientsLink = document.createElement("a");
+      liItemIngredientsLink.innerText = "show ingredients list";
+      liItemIngredientsLink.classList.add ("inner-link");
+      // create ingredients info
+      let liItemIngredients = document.createElement("p");
+      liItemIngredients.innerText = items[i].recipe.ingredientLines.toString();
+      liItemIngredients.classList.add("hide-ingredients");
+      // create show health labels link
+      let liItemHealthInfoLink = document.createElement("a");
+      liItemHealthInfoLink.innerText = "show health info";
+      liItemHealthInfoLink.classList.add ("inner-link");
+      liItemHealthInfoLink.href = "showItems()";
+      // ISSUE HERE liItemHealthInfoLink.addEventListener('click', showItems);
+      // create health labels info
+      let liItemHealthInfo = document.createElement("p");
+      liItemHealthInfo.innerText = `${Math.floor(items[i].recipe.calories)} calories; ${items[i].recipe.healthLabels.toString()}`;
+      liItemHealthInfo.classList.add("hide-health-info");
+      console.log(liItemHealthInfo);
       // create link to recipe
       let liItemLink = document.createElement("a");
       liItemLink.innerText = "click for full recipe";
@@ -68,10 +90,18 @@ async function fetchRecipe(food) {
       liItemLink.href=items[i].recipe.url;
       // add to DOM
       ulRecipeList.appendChild(liSection);
-      liSection.appendChild(liItem);
       liSection.appendChild(liItemImage);
-      liSection.appendChild(liItemLink);
+      liSection.appendChild(liSubSection);
+      liSubSection.appendChild(liItem);
+      liSubSection.appendChild(liItemIngredientsLink);
+      liSubSection.appendChild(liItemIngredients);
+      liSubSection.appendChild(liItemHealthInfoLink);
+      liSubSection.appendChild(liItemHealthInfo);
+      liSubSection.appendChild(liItemLink);
     }
   }
-  
-  
+  // ISSUE HERE
+  // function showItems(){
+  // liItemHealthInfo.classList.remove;
+  // liItemHealthInfoLink.innerText = "hide health info"
+  // }
