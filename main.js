04fb80c3e61687fb819475713c9e05f8d9c63a37
Becks Maybury <becksmaybury@hotmail.co.uk>
2021-05-13 19:04:46 +0100

    changed layout

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 const YOUR_APP_ID = "8840ad65"
 const YOUR_APP_KEY = "d1dcc8017a2e89c63e8563b289086fdc"
-// const requestUrl = `https://api.edamam.com/search?qkale&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
-// const requestUrl = `https://api.edamam.com/search?q  foodToSearch.value &app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
-// request needs q = string (query text)
-// const requestUrl = `https://api.edamam.com/search?qkale&app_id=8840ad65&app_key=d1dcc8017a2e89c63e8563b289086fdc`
-
-console.log("recipe hunting");
 
 let foodToSearch = null;
 let button = document.querySelector('#recipe-button');
 let input = document.querySelector('#food-input');
-button.addEventListener('click', createRecipeList);
+button.addEventListener('click', createRecipeListing);
 
 // async function handleRecipeClick() {
   // let recipe = await fetchRecipe(foodToSearch);
@@ -34,7 +28,7 @@ function handleFoodChange() {
 //--- write your code below ---
 
 async function fetchRecipe(food) {
-  let response = await fetch(`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`);
+  let response = await fetch(`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&time=1%2B`);
   let result = await response.json();
   let recipeList = await result.hits;
   // console.log(recipeList);
@@ -44,11 +38,10 @@ async function fetchRecipe(food) {
 
 let ulRecipeList = document.querySelector('#recipe-list');
 
-async function createRecipeList() {
+async function createRecipeListing() {
     let items = await fetchRecipe(foodToSearch);
     console.log(items);
     for (let i=0; i<items.length; i++){
-      console.log(items[i].recipe.label);
       // create recipe section
       let liSection = document.createElement("section");
       liSection.classList.add("item-section");
@@ -63,27 +56,34 @@ async function createRecipeList() {
       liItem.innerText = items[i].recipe.label;
       liItem.classList.add("item-header");
       liItem.id = i;
-      console.log("list item added");
+      // create servings info
+      let liItemServings = document.createElement("span");
+      liItemServings.innerText = "Serves: " + items[i].recipe.yield;
+      // create calories info
+      let liItemTotalCalories = (items[i].recipe.calories/items[i].recipe.yield);
+      let liItemCalories = document.createElement("span");
+      liItemCalories.innerText = "Calories per serving: "+ Math.floor(liItemTotalCalories);
+      // create time info
+      let liItemTime = document.createElement("span");
+      liItemTime.innerText = "Time (mins): " + items[i].recipe.totalTime;
       // create show ingredients link
-      let liItemIngredientsLink = document.createElement("a");
-      liItemIngredientsLink.innerText = "show ingredients list";
-      liItemIngredientsLink.classList.add ("inner-link");
+        // let liItemIngredientsLink = document.createElement("a");
+        // liItemIngredientsLink.innerText = "show ingredients list";
+        // liItemIngredientsLink.classList.add ("inner-link");
       // create ingredients info
-      let liItemIngredients = document.createElement("p");
-      liItemIngredients.innerText = items[i].recipe.ingredientLines.toString();
-      liItemIngredients.classList.add("hide-ingredients");
+        // let liItemIngredients = document.createElement("p");
+        // liItemIngredients.innerText = items[i].recipe.ingredientLines.toString();
+       // liItemIngredients.classList.add("hide-ingredients");
       // create show health labels link
-      let liItemHealthInfoLink = document.createElement("a");
-      liItemHealthInfoLink.innerText = "show health info";
-      liItemHealthInfoLink.classList.add ("inner-link");
-      liItemHealthInfoLink.href = "showItems()";
-      // ISSUE HERE liItemHealthInfoLink.addEventListener('click', showItems);
-      // create health labels info
-      let liItemHealthInfo = document.createElement("p");
-      liItemHealthInfo.innerText = `${Math.floor(items[i].recipe.calories)} calories; ${items[i].recipe.healthLabels.toString()}`;
-      liItemHealthInfo.classList.add("hide-health-info");
-      console.log(liItemHealthInfo);
-      // create link to recipe
+        // let liItemHealthInfoLink = document.createElement("a");
+        // liItemHealthInfoLink.innerText = "show health info";
+        // liItemHealthInfoLink.classList.add ("inner-link");
+        // liItemHealthInfoLink.href = "showItems()";
+        // ISSUE HERE 
+        // liItemHealthInfoLink.addEventListener('click', showItems);
+        // create cuisine type info
+        // liItemCuisineTypeInfo.classList.add("hide-health-info");
+        // create link to recipe
       let liItemLink = document.createElement("a");
       liItemLink.innerText = "click for full recipe";
       liItemLink.classList.add("outside-link");
@@ -93,15 +93,23 @@ async function createRecipeList() {
       liSection.appendChild(liItemImage);
       liSection.appendChild(liSubSection);
       liSubSection.appendChild(liItem);
-      liSubSection.appendChild(liItemIngredientsLink);
-      liSubSection.appendChild(liItemIngredients);
-      liSubSection.appendChild(liItemHealthInfoLink);
-      liSubSection.appendChild(liItemHealthInfo);
+      liSubSection.appendChild(liItemServings);
+      liSubSection.appendChild(liItemCalories);
+      liSubSection.appendChild(liItemTime);
+      // liSubSection.appendChild(liItemIngredientsLink);
+      // liSubSection.appendChild(liItemIngredients);
+      // liSubSection.appendChild(liItemHealthInfoLink);
+      if(items[i].recipe.cuisineType!= undefined) {
+        let liItemCuisineTypeInfo = document.createElement("span");
+        liItemCuisineTypeInfo.innerText = "Cuisine type: " + items[i].recipe.cuisineType;
+        liSubSection.appendChild(liItemCuisineTypeInfo);
+        console.log("blah");
+      }
       liSubSection.appendChild(liItemLink);
     }
   }
   // ISSUE HERE
-  // function showItems(){
-  // liItemHealthInfo.classList.remove;
-  // liItemHealthInfoLink.innerText = "hide health info"
-  // }
+  function showItems(){
+  liItemHealthInfo.classList.remove;
+  liItemHealthInfoLink.innerText = "hide health info"
+  }
